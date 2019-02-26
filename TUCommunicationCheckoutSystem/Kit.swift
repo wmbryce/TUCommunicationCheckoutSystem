d4b4976d85bb0c64fd3f08c5ec801819c09f72f4
Michael Bryce <michaelbryce@me.com>
2019-02-25 21:26:52 -0600

    Added NScoding to Kit Class

--- TUCommunicationCheckoutSystem/Kit.swift
+++ TUCommunicationCheckoutSystem/Kit.swift
@@ -10,8 +10,46 @@ import UIKit
 import Foundation
 import os.log
 
-class Kit: NSObject{
+class Kit: NSObject, NSCoding {
     
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(kitName, forKey: PropertyKey.kitName)
+        aCoder.encode(items, forKey: PropertyKey.items)
+        aCoder.encode(checkIn, forKey: PropertyKey.checkIn)
+        aCoder.encode(checkOut, forKey: PropertyKey.checkOut)
+        aCoder.encode(lastUsers, forKey: PropertyKey.lastUsers)
+        aCoder.encode(available, forKey: PropertyKey.available)
+    }
+    
+    init?(kitName:String, items:Array<(Int,String)>, checkIn: Int, checkOut: Int, lastUsers: Array<Int>, available: Bool){
+        
+        self.kitName = kitName
+        self.items = items
+        self.checkIn = checkIn
+        self.checkOut = checkOut
+        self.lastUsers = lastUsers
+        self.available = available
+        
+    }
+    
+    required convenience init?(coder aDecoder:NSCoder){
+        guard let name = aDecoder.decodeObject(forKey:PropertyKey.kitName) as? String else {
+            os_log("unable to decode the name for a Meal oject.", log: OSLog.default, type:.debug)
+            return nil
+            
+        }
+        let items = aDecoder.decodeObject(forKey: PropertyKey.items) as? Array<(Int,String)>
+        
+        let checkIn = aDecoder.decodeInteger(forKey: PropertyKey.checkIn)
+        
+        let checkOut = aDecoder.decodeInteger(forKey: PropertyKey.checkOut)
+        
+        let lastUsers = aDecoder.decodeObject(forKey: PropertyKey.lastUsers) as? Array<Int>
+        
+        let available = aDecoder.decodeBool(forKey: PropertyKey.available)
+        
+        self.init(kitName: name, items: items!, checkIn: checkIn, checkOut: checkOut, lastUsers: lastUsers!, available: available)
+    }   ;
     // Properties
     
     var kitName: String
@@ -19,41 +57,33 @@ class Kit: NSObject{
     var checkIn: Int
     var checkOut: Int
     var lastUsers: Array<Int>
-    
-    // Archiving Paths
-    
-    static let DocumentsDirectory = FileManager().urls( for: .documentDirectory, in:.userDomainMask).first!
-    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("Kits")
+    var available: Bool
     
     // Types
     
     struct PropertyKey {
         static let kitName = "name"
-        static let items = "description"
+        static let items = "items"
         static let checkIn = "checkIn"
         static let checkOut = "checkOut"
         static let lastUsers = "LastUsers"
+        static let available = "avaliable"
     }
     
+    //Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("Kits")
+    
     //Initialization
-    init?(kitName:String, items:Array<(Int,String)>){
-        
-        self.kitName = kitName
-        self.items = items
-        self.checkIn = 000000
-        self.checkOut = 000000
-        self.lastUsers = []
-        
-    }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
