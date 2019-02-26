6e67147447271a5d55bf1867c637a85408b367bb
Michael Bryce <michaelbryce@me.com>
2019-02-25 21:46:03 -0600

    Added inventory Table view Controller and Inventory Table Cell

---
+++ TUCommunicationCheckoutSystem/InventoryTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  InventoryTableViewCell.swift
+//  TUCommunicationCheckoutSystem
+//
+//  Created by Michael Bryce on 2/25/19.
+//  Copyright © 2019 CheckoutGurus. All rights reserved.
+//
+
+import UIKit
+
+class InventoryTableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
