ca6aee514468f39256c3cc9b121ee67e79c36ba9
Michael Bryce <michaelbryce@me.com>
2019-03-24 17:53:15 -0500

    Added MasterManage viewController

--- TUCommunicationCheckoutSystem/ManageUserControllers/MasterManageUserViewController.swift
+++ TUCommunicationCheckoutSystem/ManageUserControllers/MasterManageUserViewController.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class MasterManageUserViewController: UISplitViewController {
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self as? UISplitViewControllerDelegate
+        self.preferredDisplayMode = .allVisible
+        guard let leftNavController = self.viewControllers.first as? UINavigationController,
+            
+            let UserTableView = leftNavController.topViewController as? UserTableViewController,
+            
+            let detailViewController = self.viewControllers.last as? UserDetailViewController
+            else{
+                fatalError()
+        }
+        //let firstKit = InventoryTableView.kits.first
+        //detailViewController.kitOfInterest = firstKit ?? Kit(kitNumber: "Error", items: [0,0,0,0,0,0], checkIn: "", checkOut: "", lastUsers: [], available: false)
+        UserTableView.selectionDelegate = detailViewController
+        // Do any additional setup after loading the view.*/
+        
+    }
 }
