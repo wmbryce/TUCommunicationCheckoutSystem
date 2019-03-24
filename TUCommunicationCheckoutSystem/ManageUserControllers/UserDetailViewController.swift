7522dced2af07bd62a8da73b47a40cdfc00b8523
Josh King <joshking@joshs-macbook-air-5.local>
2019-03-24 18:25:37 -0500

    UserDetails view controller -Josh King

--- TUCommunicationCheckoutSystem/ManageUserControllers/UserDetailViewController.swift
+++ TUCommunicationCheckoutSystem/ManageUserControllers/UserDetailViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class UserDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var UserNameLabel: UILabel!
+    
+    @IBOutlet weak var TrinityIDLabel: UILabel!
+    @IBOutlet weak var EmailLabel: UILabel!
+    @IBOutlet weak var CourseLabel: UILabel!
+    @IBOutlet weak var AuthorizedLabel: UILabel!
+    @IBOutlet weak var AdminLabel: UILabel!
+    
+    @IBOutlet weak var IDvalue: UILabel!
+    @IBOutlet weak var EmailValue: UILabel!
+    @IBOutlet weak var CourseValue: UILabel!
+    @IBOutlet weak var AuthorizedSwitch: UISwitch!
+    @IBOutlet weak var AdminSwitch: UISwitch!
+   
+    var userOfInterest: User? {
+        didSet{
+            refershUI()
+        }
     }
+
+    func refershUI(){
+        loadViewIfNeeded()
+        let name = userOfInterest?.name
+        UserNameLabel.text = name!
+        let ID = userOfInterest?.ID_number
+        IDvalue.text = String(ID!)
+        let email = userOfInterest?.email
+        EmailValue.text = email!
+        let course = userOfInterest?.classId
+        CourseValue.text = course!
+        let isAdmin = userOfInterest?.isAdmin
+        AdminSwitch.setOn(isAdmin!,animated: false)
+        let isAuthorized = userOfInterest?.authorized
+        AuthorizedSwitch.setOn(isAuthorized!,animated:false)
+    }
+
     
 
     /*
