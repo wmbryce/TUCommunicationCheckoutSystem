e92f60ceeac64572a8bb085ce4ebe33b61773bc8
Michael Bryce <michaelbryce@me.com>
2019-03-24 16:45:08 -0500

    Adding pod files

---
+++ Pods/Target Support Files/Pods-TUCommunicationCheckoutSystemTests/Pods-TUCommunicationCheckoutSystemTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TUCommunicationCheckoutSystemTests : NSObject
+@end
+@implementation PodsDummy_Pods_TUCommunicationCheckoutSystemTests
+@end
