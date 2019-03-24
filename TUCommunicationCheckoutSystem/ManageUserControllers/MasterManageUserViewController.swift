f1fc70a65b3fdf9e8999f3ff724bf18d65d80d00
Michael Bryce <michaelbryce@me.com>
2019-03-24 17:00:55 -0500

    Added all files to work on

---
+++ TUCommunicationCheckoutSystem/ManageUserControllers/MasterManageUserViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MasterManageUserViewController.swift
+//  TUCommunicationCheckoutSystem
+//
+//  Created by Michael Bryce on 3/24/19.
+//  Copyright © 2019 CheckoutGurus. All rights reserved.
+//
+
+import UIKit
+
+class MasterManageUserViewController: UISplitViewController {
+
+}
---
+++ TUCommunicationCheckoutSystem/ManageUserControllers/UserDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserDetailViewController.swift
+//  TUCommunicationCheckoutSystem
+//
+//  Created by Michael Bryce on 3/24/19.
+//  Copyright © 2019 CheckoutGurus. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
